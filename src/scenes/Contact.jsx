89f1508120c17c79ee6737564e9babaff54c0d72
Jalon Lewis <jalonlewis123@gmail.com>
2023-01-01 16:35:17 -0600

    fixed contact heading animation on small screens

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -49,8 +49,8 @@ const Contact = () => {
           viewport={{ once: true, amount: 0.5 }}
           transition={{ duration: 0.5 }}
           variants={{
-            hidden: { opacity: 0, y: 50 },
-            visible: { opacity: 1, y: 0 },
+            hidden: { opacity: 0, x: 50 },
+            visible: { opacity: 1, x: 0 },
           }}
           className="basis-1/2 flex justify-center"
         >
